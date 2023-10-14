c0e9e02448e3fb56a8f608656effccb9fc476008
Siva Arumugam <sivashankar@bluejeans.com>
2023-10-14 13:43:43 +0530

    Few dummy classes

---
+++ app/src/main/java/com/siva/servicefinderapp/model/ServicesManager.kt
@@ -0,0 +1,8 @@
+/*
+ * Created by sarumugam on 14/10/23.
+ * Copyright (c) 2023 Blue Jeans Network, Inc. All rights reserved.
+ */
+package com.siva.servicefinderapp.model
+
+class ServicesManager {
+}
---
+++ app/src/main/java/com/siva/servicefinderapp/model/UserManager.kt
@@ -0,0 +1,14 @@
+/*
+ * Created by sarumugam on 14/10/23.
+ * Copyright (c) 2023 Blue Jeans Network, Inc. All rights reserved.
+ */
+package com.siva.servicefinderapp.model
+
+class UserManager {
+    // login
+        // Api to login and
+    // isLoggedIn
+    // LoggedInRole
+    // logout
+        // Api to login and
+}
