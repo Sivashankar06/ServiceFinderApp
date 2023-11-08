5e44d9e4f4bbe81d9330dd47b7a201266e29e4d2
Siva Arumugam <sivashankar@bluejeans.com>
2023-11-08 22:08:39 +0530

    Login screen UI

--- app/src/main/java/com/siva/servicefinderapp/MainActivity.kt
+++ app/src/main/java/com/siva/servicefinderapp/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.siva.servicefinderapp.login.LoginForm
 import com.siva.servicefinderapp.ui.theme.ServiceFinderAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +17,9 @@ class MainActivity : ComponentActivity() {
             ServiceFinderAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    LoginForm()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ServiceFinderAppTheme {
-        Greeting("Android")
     }
 }
